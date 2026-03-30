babe99389b338a432a1116f70ebbfaaa39ee24ce
wagnerwilliam <wagnerveleswilliam@alumnos.cei.es>
2026-03-30 22:50:35 +0200

    Agrega componente para el header y estilos basicos css

--- js/componentes/header.js
+++ js/componentes/header.js
@@ -1,16 +1,57 @@
-// Verificar el uso de web components
+/**
+ * Componente Web personalizado que representa el encabezado del sitio
+ */
 class Header extends HTMLElement {
-  connectedCallback() {
-    const title = this.getAttribute("title") || "Título por defecto";
 
+  constructor() {
+    super()
+    /**
+     * Lista de elementos de navegación que se renderizan en el menú
+     * @type {{ name: string, href: string }[]}
+     */
+    this.navItems = [
+      { name: "Inicio", href: "/index.html" },
+      { name: "Trabajos", href: "/plantillas/trabajos.html" },
+      { name: "Tienda", href: "/plantillas/tienda.html" },
+      { name: "Sobre Mi", href: "/plantillas/sobre_mi.html" },
+      { name: "Contacto", href: "/plantillas/contacto.html" }
+    ];
+  }
+
+  /**
+   * Método del ciclo de vida que se ejecuta cuando el componente
+   * se inserta en el DOM
+   */
+  connectedCallback() {
+    const subTitle = this.getAttribute("sub-title");
+    this.render(subTitle)
+  }
+
+  /**
+   * Renderiza el contenido HTML del header
+   * @param {string|null} subTitle - Texto opcional para el subtítulo
+   * @returns {void}
+   */
+  render(subTitle) {
+    let currentPath = window.location.pathname;
     this.innerHTML = `
-      <header>
-        <h1>${title}</h1>
+      <header class="header">
+          <h1>Javier Revelo</h1>
+          ${subTitle ? `<h2>${subTitle}</h2>` : ""}
+          <nav class="header__menu">
+              <ul>
+                  ${this.navItems.map((item) => `
+                      <li>
+                        <a href="${item.href}" 
+                        class="${currentPath === item.href ? 'agregar clase para indicar cuando se este en la pgina activo' : ''}"
+                        >${item.name}</a>
+                      </li>
+                  `).join("")}
+              </ul>
+          </nav>
       </header>
     `;
   }
 }
 
 customElements.define("my-header", Header);
-
-// veriricar crear el detail component
