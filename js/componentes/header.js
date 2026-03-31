9cba4466a2689e1ac08c797e869431a03397395c
wagnerwilliam <wagnerveleswilliam@alumnos.cei.es>
2026-03-31 13:48:12 +0200

    agrega componenetes para header y para trabajos.

--- js/componentes/header.js
+++ js/componentes/header.js
@@ -40,7 +40,7 @@ class Header extends HTMLElement {
     const isInTemplates = currentPath.includes("/plantillas/");
 
     // Ajusta la base de rutas
-    const base = isInTemplates ? "../" : "./";
+    const base = isInTemplates ? "./" : "";
     this.innerHTML = `
       <header class="header header__${theme}">
           <h1>Javier Revelo</h1>
